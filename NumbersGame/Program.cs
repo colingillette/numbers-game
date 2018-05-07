dc4eb8880fc42a367da238329e446fc3a23f887f
Colin Gillette <cgillette@mfncuso.net>
2018-05-07 16:55:43 -0500

    Robot class created

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -10,6 +10,39 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
+            string playerName;
+            int current, index, first;
+            bool playerTurn;
+            bool win = false;
+
+            Console.WriteLine("Welcome, what is your name?");
+            playerName = Console.ReadLine();
+
+            Random ran = new Random();
+            Robot opponent = new Robot("Computer");
+            Robot player = new Robot(playerName);
+
+            current = ran.Next(1000000, 9999999);
+
+            Console.WriteLine("Generating new game...");
+            Console.WriteLine($"The starting number is {current}");
+
+            first = ran.Next();
+
+            if (first % 2 == 0)
+            {
+                playerTurn = true;
+            }
+            else
+            {
+                playerTurn = false;
+            }
+
+            do
+            {
+
+            }
+            while (!win);
         }
     }
 }
---
+++ NumbersGame/Robot.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersGame
+{
+    class Robot
+    {
+        private string _name;
+
+        public Robot(string name)
+        {
+            _name = name;
+        }
+
+        public string Name
+        {
+            get { return this._name; }
+        }
+
+        public int MakeMove(int current)
+        {
+            string currentHolder = current.ToString();
+            string testHolder;
+            int test;
+            int output = -1;
+
+            for (int i = 0; i < currentHolder.Length; i++)
+            {
+                testHolder = currentHolder.Remove(i, 1);
+
+                int.TryParse(testHolder, out test);
+                
+                if (test % 4 == 0)
+                {
+                    output = test;
+                }
+            }
+
+            if (output == -1)
+            {
+                testHolder = currentHolder.Remove(1, 1);
+
+                int.TryParse(testHolder, out output);
+            }
+
+            return output;
+        }
+
+        public bool IsVictorious(int choice)
+        {
+            if (choice % 4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
